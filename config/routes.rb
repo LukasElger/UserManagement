e786adce927c63105186e827b13d501bb21214fc
Lukas Elger <lukaselger01@gmail.com>
2019-01-09 09:08:48 +0100

    Edited the "Back" button

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
 
   resources :users
   get 'edit_profile', to: 'users#edit_profile', as: :edit_profile
+  patch 'update_profile', to: 'users#update_profile', as: :update_profile
 end
