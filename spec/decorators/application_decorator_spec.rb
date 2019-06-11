c05478e3a7672ce67832b1830009714ba9e782ef
Lukas Elger <lukaselger01@gmail.com>
2019-06-11 08:21:42 +0200

    application_decorator_spec bearbeitet

--- spec/decorators/application_decorator_spec.rb
+++ spec/decorators/application_decorator_spec.rb
@@ -6,4 +6,20 @@ RSpec.describe ApplicationDecorator do
   it {
     expect(subject.print_date(Date.new(2018, 12, 10))).to eq("10.12.2018")
   }
+
+  describe "card_block_attr" do
+    context "attr present" do
+      before do
+        allow(subject.model).to receive_messages(name: "Test")
+      end
+      it { expect(subject.card_block_attr(:name)).to include("col-sm-3", "Name", "col-sm-9", "Test") }
+    end
+    context "else" do
+      before do
+        allow(subject.model).to receive_messages(name: nil)
+      end
+      it { expect(subject.card_block_attr(:name)).to include("col-sm-3", "Name", "col-sm-9", "N/A") }
+
+    end
+  end
 end
